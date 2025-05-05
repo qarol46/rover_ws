f7cf73c39fabc619bf25e15f45d1d87fcfd8b496
timofeenko-mf <timofeenko@ms-17.ru>
2025-05-05 16:36:15 +0300

    Add nav pkg, slam_toolbox, pontcloud-to-laserscan node

--- rover_description/launch/launch_sim.launch.py
+++ rover_description/launch/launch_sim.launch.py
@@ -5,11 +5,15 @@ from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventH
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.event_handlers import OnProcessExit
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
     package_name = 'rover_description'
+    gazebo_world_file = os.path.join(get_package_share_directory(package_name),'worlds','gas_station.world')
 
     # Запуск rsp.launch.py для публикации robot_description
+
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
@@ -22,15 +26,18 @@ def generate_launch_description():
     #            )]), launch_arguments={'use_sim_time': 'true'}.items()
     #)
 
-    ekf_config = os.path.join(get_package_share_directory(package_name), 'config', 'ekf.yaml')
-    robot_localization_node = Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_filter_node',
-        output='screen',
-        parameters=[ekf_config, {'use_sim_time': True}],
-        #remappings=[('odometry/filtered', 'odom')]  # Перенаправляем выходной топик
-    )
+    #ekf_config = os.path.join(get_package_share_directory(package_name), 'config', 'ekf.yaml')
+    #robot_localization_node = Node(
+    #    package='robot_localization',
+    #    executable='ekf_node',
+    #    name='ekf_filter_node',
+    #    output='screen',
+    #    parameters=[ekf_config, {'use_sim_time': True}],
+    #    #remappings=[('odometry/filtered', 'odom')]  # Перенаправляем выходной топик
+    #)
+
+
+    # Пути к файлам запуска
 
     rviz_config_file = os.path.join(get_package_share_directory(package_name), 'config', 'description.rviz')
     start_rviz_cmd = Node(
@@ -52,7 +59,36 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+            launch_arguments={'world': gazebo_world_file}.items()
     )
+   
+    translate = Node(
+            package='pointcloud_to_laserscan',
+            executable='pointcloud_to_laserscan_node',
+            name='pointcloud_to_laserscan',
+            parameters=[{
+                'target_frame': 'velodyne',
+                'transform_tolerance': 0.01,
+                'min_height': 0.0,  # Lowered to detect ground obstacles
+                'max_height': 10.0,
+                'angle_min': -1.5708,  # -M_PI/2
+                'angle_max': 1.5708,  # M_PI/2
+                'angle_increment': 0.01745,  # ~1 degree resolution
+                'scan_time': 0.01,
+                'range_min': 0.9,
+                'range_max': 30.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            remappings=[
+                ('/cloud_in', '/velodyne_points'),  # Input pointcloud
+                ('/scan', '/scan') # Output laserscan
+            ]
+        )
+    #navigation = IncludeLaunchDescription(
+    #    PythonLaunchDescriptionSource([os.path.join(
+    #        get_package_share_directory('rover_navigation'), 'launch', 'slam.launch.py')])
+    #)
 
     # Спавн робота в Gazebo
     spawn_entity = Node(
@@ -109,9 +145,11 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
-        robot_localization_node,
+        #robot_localization_node,
         start_rviz_cmd,
         control_node,
+        translate,
+        #navigation,
         delay_diff_drive_spawner,
         delay_joint_broad_spawner,
         #joystick,
